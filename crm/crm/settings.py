6c0f09ee869968935745da40f37d206f71d0ccf2
Hamit Ekşi <hamiteksi@192.168.1.105>
2023-08-01 21:06:27 +0300

    Staticroot Eklendi 2

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -142,7 +142,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "musteri/static"),
     os.path.join(BASE_DIR, "img"),
 ]
 STATIC_URL = "/static/"
