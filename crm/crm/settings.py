1f5033c23b496822765801a4b0f6c2e9035171dd
Hamit Ekşi <hamiteksi@192.168.1.105>
2024-03-03 00:35:27 +0300

    Heroku icin hazir7

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     # os.path.join(BASE_DIR, "images"),
 ]
 STATIC_URL = "/static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
