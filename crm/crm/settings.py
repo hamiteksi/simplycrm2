5b8a5eb4d161a117a792d40f2a183b78e77c2a4a
Hamit Ekşi <hamiteksi@192.168.1.105>
2024-03-03 01:20:42 +0300

    Uygulama calisma sorunlari cozuldu4

--- crm/crm/settings.py
+++ crm/crm/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "static"),
     # os.path.join(BASE_DIR, "images"),
 ]
 STATIC_URL = "/static/"
